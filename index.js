955c4321518e76eaf848a4fae66b23e308a6fedd
LucasAlktr <lucasbenington@hotmail.com>
2024-03-07 19:06:55 -0500

    background and font adjustments

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
-const body = document.getElementById('body');
-const lightModeToggle = document.getElementById('light-mode-toggle');
 const hamMenuBtn = document.querySelector('.header-main-ham-menu-cont')
 const smallMenu = document.querySelector('.header-sm-menu')
 const headerHamMenuBtn = document.querySelector('.header-main-ham-menu')
-const headerHamMenuCloseBtn = document.querySelector(  '.header-main-ham-menu-close')
+const headerHamMenuCloseBtn = document.querySelector('.header-main-ham-menu-close')
 const headerSmallMenuLinks = document.querySelectorAll('.header-sm-menu-link')
 
-lightModeToggle.addEventListener('click', () => {
-  body.classList.toggle('light-mode');
-
-  if (body.classList.contains('light-mode')) {
-      lightModeToggle.src = 'img/icons-light-on.png';
-  } else {
-      lightModeToggle.src = 'img/icons-light-off.png';
-  }
-});
-
 hamMenuBtn.addEventListener('click', () => {
-  if (smallMenu.classList.contains('header-sm-menu--active')) {
-    smallMenu.classList.remove('header-sm-menu--active')
-  } else {
-    smallMenu.classList.add('header-sm-menu--active')
-  }
-  if (headerHamMenuBtn.classList.contains('disp-none')) {
-    headerHamMenuBtn.classList.remove('disp-none')
-    headerHamMenuCloseBtn.classList.add('disp-none')
-  } else {
-    headerHamMenuBtn.classList.add('disp-none')
-    headerHamMenuCloseBtn.classList.remove('disp-none')
-  }
+if (smallMenu.classList.contains('header-sm-menu--active')) {
+  smallMenu.classList.remove('header-sm-menu--active')
+} else {
+  smallMenu.classList.add('header-sm-menu--active')
+}
+if (headerHamMenuBtn.classList.contains('disp-none')) {
+  headerHamMenuBtn.classList.remove('disp-none')
+  headerHamMenuCloseBtn.classList.add('disp-none')
+} else {
+  headerHamMenuBtn.classList.add('disp-none')
+  headerHamMenuCloseBtn.classList.remove('disp-none')
+}
 })
 
 for (let i = 0; i < headerSmallMenuLinks.length; i++) {
-  headerSmallMenuLinks[i].addEventListener('click', () => {
-    smallMenu.classList.remove('header-sm-menu--active')
-    headerHamMenuBtn.classList.remove('disp-none')
-    headerHamMenuCloseBtn.classList.add('disp-none')
-  })
+headerSmallMenuLinks[i].addEventListener('click', () => {
+  smallMenu.classList.remove('header-sm-menu--active')
+  headerHamMenuBtn.classList.remove('disp-none')
+  headerHamMenuCloseBtn.classList.add('disp-none')
+})
 }
 
-// ---
-const headerLogoConatiner = document.querySelector('.header-logo-container')
+const headerLogoContainer = document.querySelector('.header-logo-container')
 
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = '/'
+headerLogoContainer.addEventListener('click', () => {
+location.href = '/'
 });
-
